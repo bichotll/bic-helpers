eeef53cb010d094f93808d17e02afd83d73f7aa8
Victor Demin <demmbox@gmail.com>
2014-10-30 23:02:51 +0100

    Update Strings.php

--- src/Bic/Helpers/Strings.php
+++ src/Bic/Helpers/Strings.php
@@ -22,5 +22,15 @@ class Strings {
         $slug = preg_replace('/[^\w-]+/', '', $slug);
         return $slug;
     }
+    
+    /**
+     * Get YouTube code from YouTube link
+     * @param string link
+     * @return string|null YouTube code or false
+     */
+    public static function getYouTubeCode($link) {
+        preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $link, $matches);
+        return isset($matches[0]) ? $matches[0] : null;
+    }
 
 }
